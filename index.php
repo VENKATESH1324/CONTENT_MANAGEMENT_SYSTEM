23f7ca29352ec6c1d5bbcb783074045642d766b5
VENKATESH1324 <venkatesh.1324@gmail.com>
2017-05-16 15:25:34 -0600

    modified index.php

--- index.php
+++ index.php
@@ -10,17 +10,30 @@
         <div class="row">
 
             <!-- Blog Entries Column -->
+            
             <div class="col-md-8">
 <?php
     $query = "SELECT * FROM posts";
     $result = mysqli_query($connection,$query);
     while($row = mysqli_fetch_assoc($result)){
+        
         $post_id = $row['post_id'];
         $post_title = $row['post_title'];
         $post_author = $row['post_author'];
         $post_date = $row['post_date'];
         $post_image = $row['post_image'];
         $post_content = substr($row['post_content'],0,150);
+        $post_status = $row['post_status'];
+
+        
+if($post_status !== 'published'){
+
+           // echo "<h1 class='text-center'>SORRY, NO POSTS YET </h1>";
+
+
+        } else { 
+
+
 
 ?>
    <!-- First Blog Post -->
@@ -38,11 +51,12 @@
                 <hr><a href="post.php?p_id=<?php echo $post_id; ?>">
                 <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt=""></a>
                 <hr>
+                
                 <p><?php echo $post_content ?></p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
                 <hr>             
         
-<?php    } ?>
+<?php    }  } ?>
 
 
     </div>
@@ -50,7 +64,8 @@
         <!-- Blog Sidebar Widgets Column -->
             <?php include "includes/sidebar.php"; ?>
         </div>
+     <hr>
         <!-- /.row -->
-        <hr>
+       
     
 <?php include "includes/footer.php"; ?>
